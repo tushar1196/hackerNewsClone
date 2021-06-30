0347506c299816c6535e1a21926ed2a784717216
Tushar Chavhan <tusharchavhan111196@gmail.com>
2021-06-30 16:09:22 +0530

    Initial Commit

---
+++ src/main/java/com/mountblue/hackernews/HackernewsApplication.java
@@ -0,0 +1,13 @@
+package com.mountblue.hackernews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackernewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackernewsApplication.class, args);
+	}
+
+}
